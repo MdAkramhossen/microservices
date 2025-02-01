477ee7f303f170797efcf9ac92341081eddc338b
MdAkramhossen <mdakramhossen613@gmail.com>
2025-02-02 03:37:25 +0600

    eureka-server-config-completed

--- student-service/src/main/java/com/logrex/student/StudentApplication.java
+++ student-service/src/main/java/com/logrex/student/StudentApplication.java
@@ -2,14 +2,16 @@ package com.logrex.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class MicroServiceApplication {
+
+public class StudentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroServiceApplication.class, args);
+        SpringApplication.run(StudentApplication.class, args);
 
 
     }
--- student-service/src/main/java/com/logrex/student/feignclients/AddressFeignClients.java
+++ student-service/src/main/java/com/logrex/student/feignclients/AddressFeignClients.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "${address.service.url}", value = "address-feign-client",
+///@FeignClient(url = "${address.service.url}", value = "address-feign-client",path = "/api/address")
+
+@FeignClient( value = "ADDRESS-SERVICE",
         path = "/api/address")
 
 public interface AddressFeignClients {
