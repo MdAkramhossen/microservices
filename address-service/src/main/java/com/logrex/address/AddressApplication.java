d52ed6f3477eb44ebe8a09a0558a56dbdf4d6377
MdAkramhossen <mdakramhossen613@gmail.com>
2025-02-04 12:16:28 +0600

    circuit breaker sucessfully configured

--- address-service/src/main/java/com/logrex/address/AddressApplication.java
+++ address-service/src/main/java/com/logrex/address/AddressApplication.java
@@ -12,7 +12,7 @@ public class AddressApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressApplication.class, args);
 
-		System.out.println("hello address");
+
 	}
 
 }
